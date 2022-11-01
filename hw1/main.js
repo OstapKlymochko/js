9df704f431b375edafb0aadc4056d54f87d61a17
OstapKlymochko <klymochkoo@gmail.com>
2022-11-01 18:36:36 +0200

    Initial commit

---
+++ hw1/main.js
@@ -0,0 +1,26 @@
+let a='hello';
+let b='owu';
+let c = 'com';
+console.log(a+' '+b+' '+c+'.');
+a=1;
+b=10;
+c=-999;
+console.log(a,b,c);
+const Pi = 3.14;
+a=2.7;
+b=16;
+console.log(Pi,a,b);
+a=true
+b=false
+console.log(a,b)
+let firstName = 'Остап'
+let middleName ='Климочко'
+let lastName = 'Тарасович'
+console.log(middleName + ' '+firstName+' '+lastName+'.\n');
+a = 100;
+b = '100';
+c = true;
+console.log(a,typeof a, b,typeof b,
+    c, typeof c)
+let age = prompt('Input your age:', 100000000000000)
+alert(`Your age: ${age}!`)
